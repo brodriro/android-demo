78d5dce228162358ce7c8c0a5671ba4aa5d4618c
Brian Rodriguez <rzknairb@gmail.com>
2019-08-28 20:44:42 -0500

    add bottom navigation

--- app/src/main/java/me/rzknairb/demoapp/views/home/HomeActivity.java
+++ app/src/main/java/me/rzknairb/demoapp/views/home/HomeActivity.java
@@ -1,20 +1,41 @@
 package me.rzknairb.demoapp.views.home;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import me.rzknairb.demoapp.R;
 import me.rzknairb.demoapp.views.BaseActivity;
 
-public class HomeActivity extends BaseActivity implements HomePresenter.View {
+public class HomeActivity extends BaseActivity implements HomePresenter.View,BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Inject
     HomePresenter homePresenter;
+
+    @BindView(R.id.bottom_navigation_main)
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initViews();
+    }
+
+    private void initViews() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
